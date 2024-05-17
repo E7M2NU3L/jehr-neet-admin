f74f9997e7521991c2847a1a9716e6007fe417c5
E7M2NU3L <aemmanuel685210@gmail.com>
2024-05-17 13:18:38 +0530

    made some styling changes

--- app/(root)/home/home.tsx
+++ app/(root)/home/home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='min-h-[90vh] bg-slate-200'>Home</div>
+    <div className='min-h-[90vh] bg-dark-5'>Home</div>
   )
 }
 
